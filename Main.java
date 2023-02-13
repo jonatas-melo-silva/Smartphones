36165a148093b3fcfa400515cc9e74995aade1ab
Jonatas Melo Silva <jonatas891024@gmail.com>
2023-02-13 21:45:49 +0000

    test: testa a classe App

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import main.java.com.app.App;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // criar um App
+    App app = new App("Facebook", "Meta", 100.0f);
+    System.out.println(app.getTitulo());
   }
 }
